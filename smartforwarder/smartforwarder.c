e249754a0e7dc682282d6b0e7ca9fab5b4d86840
SAMYAK <samyakjain7580@gmail.com>
2025-07-09 21:15:52 +0530

    Added code for Launching processing logic on each core Port forwarding Printing stats Mac updating

--- smartforwarder/smartforwarder.c
+++ smartforwarder/smartforwarder.c
@@ -105,6 +105,198 @@ signal_handler(int signum)
         }
 }
 
+/* Print out statistics on packets dropped */
+static void
+print_stats(void)
+{
+        uint64_t total_packets_dropped, total_packets_tx, total_packets_rx;
+        unsigned portid;
+
+        total_packets_dropped = 0;
+        total_packets_tx = 0;
+        total_packets_rx = 0;
+
+        const char clr[] = { 27, '[', '2', 'J', '\0' };
+        const char topLeft[] = { 27, '[', '1', ';', '1', 'H','\0' };
+
+                /* Clear screen and move to top left */
+        printf("%s%s", clr, topLeft);
+
+        printf("\nPort statistics ====================================");
+
+        for (portid = 0; portid < RTE_MAX_ETHPORTS; portid++) {
+                /* skip disabled ports */
+                if ((l2fwd_enabled_port_mask & (1 << portid)) == 0)
+                        continue;
+                printf("\nStatistics for port %u ------------------------------"
+                           "\nPackets sent: %24"PRIu64
+                           "\nPackets received: %20"PRIu64
+                           "\nPackets dropped: %21"PRIu64,
+                           portid,
+                           port_statistics[portid].tx,
+                           port_statistics[portid].rx,
+                           port_statistics[portid].dropped);
+
+                total_packets_dropped += port_statistics[portid].dropped;
+                total_packets_tx += port_statistics[portid].tx;
+                total_packets_rx += port_statistics[portid].rx;
+        }
+        printf("\nAggregate statistics ==============================="
+                   "\nTotal packets sent: %18"PRIu64
+                   "\nTotal packets received: %14"PRIu64
+                   "\nTotal packets dropped: %15"PRIu64,
+                   total_packets_tx,
+                   total_packets_rx,
+                   total_packets_dropped);
+        printf("\n====================================================\n");
+
+        fflush(stdout);
+}
+
+static void
+smartfwd_mac_updating(struct rte_mbuf *m, unsigned dest_portid)
+{
+        struct rte_ether_hdr *eth;
+        void *tmp;
+
+        eth = rte_pktmbuf_mtod(m, struct rte_ether_hdr *);
+
+        /* 02:00:00:00:00:xx */
+        tmp = &eth->dst_addr.addr_bytes[0];
+        *((uint64_t *)tmp) = 0x000000000002 + ((uint64_t)dest_portid << 40);
+
+        /* src addr */
+        rte_ether_addr_copy(&smartfwd_ports_eth_addr[dest_portid], &eth->src_addr);
+}
+
+/* Simple forward. 8< */
+static void
+smartfwd_simple_forward(struct rte_mbuf *m, unsigned portid)
+{
+        unsigned dst_port;
+        int sent;
+        struct rte_eth_dev_tx_buffer *buffer;
+
+        dst_port = l2fwd_dst_ports[portid];
+
+        if (mac_updating)
+                smartfwd_mac_updating(m, dst_port);
+
+        buffer = tx_buffer[dst_port];
+        sent = rte_eth_tx_buffer(dst_port, 0, buffer, m);
+        if (sent)
+                port_statistics[dst_port].tx += sent;
+}
+/* >8 End of simple forward. */
+
+static void
+smartfwd_main_loop(void)
+{
+        struct rte_mbuf *pkts_burst[MAX_PKT_BURST];
+        struct rte_mbuf *m;
+        int sent;
+        unsigned lcore_id;
+        uint64_t prev_tsc, diff_tsc, cur_tsc, timer_tsc;
+        unsigned i, j, portid, nb_rx;
+        struct lcore_queue_conf *qconf;
+        const uint64_t drain_tsc = (rte_get_tsc_hz() + US_PER_S - 1) / US_PER_S *
+                        BURST_TX_DRAIN_US;
+        struct rte_eth_dev_tx_buffer *buffer;
+
+        prev_tsc = 0;
+        timer_tsc = 0;
+
+        lcore_id = rte_lcore_id();
+        qconf = &lcore_queue_conf[lcore_id];
+
+        if (qconf->n_rx_port == 0) {
+                RTE_LOG(INFO, L2FWD, "lcore %u has nothing to do\n", lcore_id);
+                return;
+        }
+
+        RTE_LOG(INFO, L2FWD, "entering main loop on lcore %u\n", lcore_id);
+        for (i = 0; i < qconf->n_rx_port; i++) {
+
+                portid = qconf->rx_port_list[i];
+                RTE_LOG(INFO, L2FWD, " -- lcoreid=%u portid=%u\n", lcore_id,
+                        portid);
+
+        }
+
+        while (!force_quit) {
+
+                /* Drains TX queue in its main loop. 8< */
+                cur_tsc = rte_rdtsc();
+
+                /*
+                 * TX burst queue drain
+                 */
+                diff_tsc = cur_tsc - prev_tsc;
+                if (unlikely(diff_tsc > drain_tsc)) {
+
+                        for (i = 0; i < qconf->n_rx_port; i++) {
+
+                                portid = smartfwd_dst_ports[qconf->rx_port_list[i]];
+                                buffer = tx_buffer[portid];
+
+                                sent = rte_eth_tx_buffer_flush(portid, 0, buffer);
+                                if (sent)
+                                        port_statistics[portid].tx += sent;
+
+                        }
+
+                        /* if timer is enabled */
+                        if (timer_period > 0) {
+
+                                /* advance the timer */
+                                timer_tsc += diff_tsc;
+
+                                /* if timer has reached its timeout */
+                                if (unlikely(timer_tsc >= timer_period)) {
+
+                                        /* do this only on main core */
+                                        if (lcore_id == rte_get_main_lcore()) {
+                                                print_stats();
+                                                /* reset the timer */
+                                                timer_tsc = 0;
+                                        }
+                                }
+                        }
+
+                        prev_tsc = cur_tsc;
+                }
+                /* >8 End of draining TX queue. */
+
+                /* Read packet from RX queues. 8< */
+                for (i = 0; i < qconf->n_rx_port; i++) {
+
+                        portid = qconf->rx_port_list[i];
+                        nb_rx = rte_eth_rx_burst(portid, 0,
+                                                 pkts_burst, MAX_PKT_BURST);
+
+                        if (unlikely(nb_rx == 0))
+                                continue;
+
+                        port_statistics[portid].rx += nb_rx;
+
+                        for (j = 0; j < nb_rx; j++) {
+                                m = pkts_burst[j];
+                                rte_prefetch0(rte_pktmbuf_mtod(m, void *));
+                                smartfwd_simple_forward(m, portid);
+                        }
+                }
+                /* >8 End of read packet from RX queues. */
+        }
+}
+
+
+static int
+smartfwd_launch_one_lcore(__rte_unused void *dummy)
+{
+        smartfwd_main_loop();
+        return 0;
+}
+
 /* display usage */
 static void
 smartfwd_usage(const char *prgname)
@@ -442,6 +634,26 @@ main(int argc, char **argv)
 	}
 
 	rte_pdump_init();
+
+        ret = 0;
+        /* launch per-lcore init on every lcore */
+        rte_eal_mp_remote_launch(smartfwd_launch_one_lcore, NULL, CALL_MAIN);
+        RTE_LCORE_FOREACH_WORKER(lcore_id) {
+                if (rte_eal_wait_lcore(lcore_id) < 0) {
+                        ret = -1;
+                        break;
+                }
+        }
+
+        RTE_ETH_FOREACH_DEV(portid) {
+                printf("Closing port %d...", portid);
+                ret = rte_eth_dev_stop(portid);
+                if (ret != 0)
+                        printf("rte_eth_dev_stop: err=%d, port=%d\n",
+                               ret, portid);
+                rte_eth_dev_close(portid);
+                printf(" Done\n");
+        }
 
         /* clean up the EAL */
         rte_eal_cleanup();
